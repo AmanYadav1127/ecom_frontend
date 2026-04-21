607d84e2da999db74a3995528852d676d2fdc01d
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-04-22 01:02:14 +0530

    small image ui interface demo

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,7 +27,7 @@ const Products = () => {
     }
 ];
     return(
-        <div className="lg-px-14 sm-px-4 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
+        <div className="lg:px-14 sm:px-4 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
           {
             isLoading ? (
                 <p>Loading...</p>
