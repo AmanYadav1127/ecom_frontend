c2679b367051645a6993509f0d2ae072b729e2ed
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-05-03 02:37:24 +0530

    Added Product Filter Interface

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,6 @@
+import { Button, FormControl, InputLabel, MenuItem, Select, Tooltip } from "@mui/material";
 import { useState } from "react";
-import { FiSearch } from "react-icons/fi";
+import { FiArrowDown, FiArrowUp, FiRefreshCw, FiSearch } from "react-icons/fi";
 const Filter = () => {
   const categories = [
     { categoryId: 1, categoryName: "Electronics" },
@@ -16,6 +17,7 @@ const Filter = () => {
 };
 
 return (
+    // Search Bar
   <div className="flex lg:flex-row flex-col-reverse lg:justify-between justify-center gap-4">
     <div className="relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full">
         <input
@@ -23,6 +25,46 @@ return (
          placeholder="Search Products"
          className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline focus:ring-2 focus:ring-blue-500" />
         <FiSearch className="absolute left-3 text-slate-800 size={20}" />
+    </div>
+    {/* CATEGORY FILTER */}
+    <div className="flex sm:flex-row flex-col gap-4 items-center">
+                    <FormControl
+            variant="outlined"
+            size="small"
+            className="text-slate-800 border-slate-700"
+            >
+            <InputLabel id="category-select-label">Category</InputLabel>
+            <Select
+                labelId="category-select-label"
+                id="category-select"
+                value={category}
+                onChange={handleCategoryChange}
+                label="Category"
+                className="min-w-[120p] text-slate-800 border-slate-700"
+            >
+                <MenuItem value="all">All</MenuItem>
+                {categories.map((cat) => (
+                <MenuItem key={cat.categoryId} value={cat.categoryName}>
+                    {cat.categoryName}
+                </MenuItem>
+                ))}
+            </Select>
+            </FormControl>
+
+            <Tooltip title="Sorted by price: asc">
+                    <Button variant="contained" 
+                        color="primary" 
+                        className="flex items-center gap-2 h-10">
+                        Sort By
+                       <FiArrowUp size={20}/>
+                    </Button>
+                </Tooltip>
+                <button 
+                className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-null"
+                >
+                    <FiRefreshCw className="font-semibold" size={16}/>
+                    <span className="font-semibold">Clear Filter</span>
+                </button>
     </div>
   </div>
 );
